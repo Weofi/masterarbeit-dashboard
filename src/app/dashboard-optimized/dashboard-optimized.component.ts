4928bf0c356ef67309dda3824e2c884dafb0d21b
Jack-Justin Drillisch <jack-justin.drillisch@datev.de>
2025-03-21 15:03:59 +0100

    feat: add search functionality to dashboard with reactive form

--- src/app/dashboard-optimized/dashboard-optimized.component.ts
+++ src/app/dashboard-optimized/dashboard-optimized.component.ts
@@ -1,16 +1,19 @@
 import {Component, inject, Input, OnInit} from '@angular/core';
 import {DataService, Person} from '../data-service/data.service';
-import {Observable} from 'rxjs';
+import {combineLatest, debounceTime, map, Observable, startWith} from 'rxjs';
 import {ScrollingModule} from '@angular/cdk/scrolling';
 import {CardComponent} from '../card/card.component';
 import {AsyncPipe} from '@angular/common';
+import {FormControl, FormsModule, ReactiveFormsModule} from '@angular/forms';
 
 @Component({
   selector: 'app-dashboard-optimized',
   imports: [
     ScrollingModule,
     CardComponent,
-    AsyncPipe
+    AsyncPipe,
+    FormsModule,
+    ReactiveFormsModule
   ],
   templateUrl: './dashboard-optimized.component.html',
   styleUrl: './dashboard-optimized.component.css'
@@ -19,20 +22,33 @@ export class DashboardOptimizedComponent implements OnInit {
   @Input() public dataSet: 1000 | 10_000 | 50_000 = 1000;
   private readonly dataService = inject(DataService);
 
-  public personData$!: Observable<Person[]>;
+  public filteredPersonData$!: Observable<Person[]>;
+  public searchForm = new FormControl;
+
 
   private getPersonData() {
     switch (this.dataSet) {
       case 1000:
         return this.dataService.get1k();
       case 10_000:
-        return  this.dataService.get10k();
+        return this.dataService.get10k();
       case 50_000:
-        return  this.dataService.get50k();
+        return this.dataService.get50k();
     }
   }
 
   ngOnInit(): void {
-    this.personData$ = this.getPersonData();
+    this.filteredPersonData$ =  combineLatest([
+      this.getPersonData(),
+      this.searchForm.valueChanges.pipe(
+        startWith(''),
+        debounceTime(300)
+      ),
+    ]).pipe(
+      map(([personData, search]) => search.length > 0 ? personData :
+        personData.filter(person =>
+          (person.first_name + person.last_name + person.email + person.gender).toLowerCase().includes(search.toLowerCase().replaceAll(" ","")))
+      ),
+    );
   }
 }
--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,36 +1,51 @@
 import {Component, inject, Input, OnInit} from '@angular/core';
 import {DataService, Person} from '../data-service/data.service';
-import {AsyncPipe} from '@angular/common';
 import {CardComponent} from '../card/card.component';
-import {Observable} from 'rxjs';
+import {combineLatest, debounceTime, map, Observable, startWith} from 'rxjs';
+import {FormControl, ReactiveFormsModule} from '@angular/forms';
+import {AsyncPipe} from '@angular/common';
 
 @Component({
   selector: 'app-dashboard',
   imports: [
-    AsyncPipe,
-    CardComponent
+    CardComponent,
+    ReactiveFormsModule,
+    AsyncPipe
   ],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.css'
 })
-export class DashboardComponent implements OnInit {
+export class DashboardComponent implements OnInit{
   @Input() public dataSet: 1000 | 10_000 | 50_000 = 1000;
   private readonly dataService = inject(DataService);
 
-  public personData$!: Observable<Person[]>;
+  public filteredPersonData$!: Observable<Person[]>;
+  public searchForm = new FormControl;
+
 
   private getPersonData() {
     switch (this.dataSet) {
       case 1000:
         return this.dataService.get1k();
       case 10_000:
-        return  this.dataService.get10k();
+        return this.dataService.get10k();
       case 50_000:
-        return  this.dataService.get50k();
+        return this.dataService.get50k();
     }
   }
 
   ngOnInit(): void {
-    this.personData$ = this.getPersonData();
+    this.filteredPersonData$ =  combineLatest([
+      this.getPersonData(),
+      this.searchForm.valueChanges.pipe(
+        startWith(''),
+        debounceTime(300)
+      ),
+    ]).pipe(
+      map(([personData, search]) => search.length > 0 ? personData :
+        personData.filter(person =>
+          (person.first_name + person.last_name + person.email + person.gender).toLowerCase().includes(search.toLowerCase().replaceAll(" ","")))
+      ),
+    );
   }
 }
