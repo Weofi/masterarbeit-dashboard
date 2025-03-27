6fa7ad2e1725ce67aa26804dc4f33ab3450de3bb
Jack-Justin Drillisch <jack-justin.drillisch@datev.de>
2025-03-27 09:29:54 +0100

    moved angular app into own folder

--- mockDataGenerator.js
+++ mockDataGenerator.js
@@ -1,7 +1,7 @@
 const fs = require('fs');
 
 // Load the existing dataset
-const data = require('./public/MOCK_DATA.json');
+const data = require('./angular-app/public/MOCK_DATA.json');
 
 // Function to increase the dataset size
 function increaseDatasetSize(data, targetSize) {
@@ -23,14 +23,14 @@ function increaseDatasetSize(data, targetSize) {
 
 // Increase to 10,000 entries
 const data10000 = increaseDatasetSize(data, 10000);
-fs.writeFileSync('./public/MOCK_DATA_10_000.json', JSON.stringify(data10000, null, 2));
+fs.writeFileSync('./angular-app/public/MOCK_DATA_10_000.json', JSON.stringify(data10000, null, 2));
 
 // Increase to 100,000 entries
 const data100000 = increaseDatasetSize(data, 100000);
-fs.writeFileSync('./public/MOCK_DATA_100_000.json', JSON.stringify(data100000, null, 2));
+fs.writeFileSync('./angular-app/public/MOCK_DATA_100_000.json', JSON.stringify(data100000, null, 2));
 
 // Increase to 1,000,000 entries
 const data1000000 = increaseDatasetSize(data, 1000000);
-fs.writeFileSync('./public/MOCK_DATA_1_000_000.json', JSON.stringify(data1000000, null, 2));
+fs.writeFileSync('./angular-app/public/MOCK_DATA_1_000_000.json', JSON.stringify(data1000000, null, 2));
 
 console.log('Datasets have been increased and saved.');
